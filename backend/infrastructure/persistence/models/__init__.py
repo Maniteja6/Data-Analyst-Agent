650d170e606899c7f9bcbbac4fd5a406dc4dfed9
Maniteja6 <maniteja669@gmail.com>
2026-07-08 13:02:05 -0700

    alembic error

--- backend/infrastructure/persistence/models/__init__.py
+++ backend/infrastructure/persistence/models/__init__.py
@@ -3,7 +3,7 @@
 from backend.infrastructure.persistence.models.agent_execution_model import AgentExecutionModel
 from backend.infrastructure.persistence.models.conversation_model import ConversationModel
 from backend.infrastructure.persistence.models.dataset_model import DatasetModel
-from backend.infrastructure.persistence.models.insight_report_model import InsightReportModel
+from backend.infrastructure.persistence.models.insight_model import InsightReportModel
 from backend.infrastructure.persistence.models.message_model import MessageModel
 from backend.infrastructure.persistence.models.session_model import SessionModel
 
